5650aeb34589c6acd86ba9b7edaae874ed487568
Chase Cockrell <rcockrel@ip079079.uvm.edu>
2025-08-19 15:06:04 -0400

    PK/PD additions

--- CPP_Hypo/Parameters.h
+++ CPP_Hypo/Parameters.h
@@ -8,10 +8,15 @@
 	const int numRules=25;
 	const int numRuleParams=17;
 
-
-
 //PK/PD variables
 const float etanerceptReduction=0.9;
 const float rilanoceptReduction=0.9;
 const float dupilumabReduction=0.9;
 const float ustekinumabReduction=0.9;
+
+const float etanerceptElimConstant=0.0069;
+const float rilanoceptElimConstant=0.0041;
+const float filgrastimElimConstant=0.1980;
+const float actimmuneElimConstant=1.0502;
+const float dupilumabElimConstant=0.0021;
+const float ustekinumabElimConstant=0.0011;
--- pkpd.cpp
+++ pkpd.cpp
@@ -12,22 +12,24 @@ using namespace std;
 extern vector<EC> ecArray;
 extern const int cellCapacity,xDim,yDim,injuryStep,parameterInput,numTimeSteps;
 extern const float etanerceptReduction,rilanoceptReduction,dupilumabReduction,ustekinumabReduction;
+extern const float etanerceptElimConstant,rilanoceptElimConstant,filgrastimElimConstant,
+                   actimmuneElimConstant,dupilumabElimConstant,ustekinumabElimConstant;
 
-//Etanercept - binds to and neutralizes TNF >90% for enture drug course; Half-life =100 h^{-1}
+//Etanercept - binds to and neutralizes TNF >90% for enture drug course; Half-life =100 h^{-1}; elim constant=0.0069
 //1) https://pmc.ncbi.nlm.nih.gov/articles/PMC3726066/
 //2) Update on the use of etanercept across a spectrum of rheumatoid disorders
 //3) Effects of three anti-TNF-α drugs: Etanercept, infliximab and pirfenidone on release of TNF-α in medium and TNF-α associated with the cell in vitro
 //   https://www.sciencedirect.com/science/article/abs/pii/S1567576908000155
 
-//rilonacept - neutralizes IL_1; half-life=168 h^{-1}
+//rilonacept - neutralizes IL_1; half-life=168 h^{-1}; elim constant=0.0041
 
-//filgrastim (Neupogen) - same as giving GCSF; increases GCSF by ~1000x, half-life = 3.5 h^{-1}
+//filgrastim (Neupogen) - same as giving GCSF; increases GCSF by ~1000x, half-life = 3.5 h^{-1}; elim constant=0.1980
 
-//Actimmune - same as giving IFNg; half-life = 0.66 h^{-1}
+//Actimmune - same as giving IFNg; half-life = 0.66 h^{-1}; elim constant=1.0502
 
-//Dupilumab (Dupixent) - inhibits IL4 signaling (does not reduce IL4) - half-life=335 h^{-1}
+//Dupilumab (Dupixent) - inhibits IL4 signaling (does not reduce IL4) - half-life=335 h^{-1}; elim constant=0.0021
 
-//Ustekinumab (Stelara) - reduces IL12 levels - half-life = 648 h^{-1}
+//Ustekinumab (Stelara) - reduces IL12 levels - half-life = 648 h^{-1}; elim constant=0.0011
 //P663 Post-ustekinumab induction IL12, IL23, and ustekinumab levels are associated with clinical response in a multi-centre prospective cohort study of Crohn’s disease patients: results from the AURORA Study including ANZIBDC Cohort
 //https://academic.oup.com/ecco-jcc/article/17/Supplement_1/i793/7010121
 
